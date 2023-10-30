cd31addf7e3c6812a0ce51a783e249a9a39f9436
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-10-30 14:10:19 +0100

    Checks if a singly linked list is cyclic

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,15 +11,13 @@ int check_cycle(listint_t *list)
 	listint_t *tortoise = list;
 	listint_t *hare = list;
 
-	if (!list || !(list->next))
+	if (!list || !list->next)
 		return (0);
 
-	hare = hare->next;
-	tortoise = tortoise->next;
-
-	while (hare && tortoise)
+	while (hare && hare->next)
 	{
 		tortoise = tortoise->next;
+		hare = hare->next->next;
 
 		if (tortoise == hare)
 			return (1);
