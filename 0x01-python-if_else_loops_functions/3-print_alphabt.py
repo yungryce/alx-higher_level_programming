814a05c53f008b7544770fb88a03e393121b9933
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-10-31 12:16:42 +0100

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 for char_code in range(ord('a'), ord('z') + 1):
     if char_code == ord('q') or char_code == ord('e'):
         continue
-    print(chr(char_code), end='')
+    print("{}".format(chr(char_code)), end='')
