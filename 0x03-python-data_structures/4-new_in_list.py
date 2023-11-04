e1b3785ad267aec6e8bc752a063bcd550a2a8e96
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-11-04 17:58:29 +0100

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+
 def new_in_list(my_list, idx, element):
+    """
+    for i in range(len(my_list)):
+        if i == idx:
+            new_list.append(element)
+        else:
+            new_list.append(my_list[i])
+    """
     if idx < 0 or idx >= len(my_list):
         return my_list
     new_list = []
@@ -9,10 +17,3 @@ def new_in_list(my_list, idx, element):
         else:
             new_list.append(element)
     return new_list
-"""
-    for i in range(len(my_list)):
-        if i == idx:
-            new_list.append(element)
-        else:
-            new_list.append(my_list[i])
-"""
