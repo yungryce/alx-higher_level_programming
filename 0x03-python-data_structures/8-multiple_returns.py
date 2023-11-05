c5075381617ab81592bef1ab4d13a9b477f284ed
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-11-05 12:10:11 +0100

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,8 @@
 def multiple_returns(sentence):
     new_tuple = len(sentence),
     for i in sentence:
+        if i == None:
+            new_tuple += (None,)
         if i == sentence[0]:
             new_tuple += (i,)
     return new_tuple
