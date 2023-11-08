a8c76108185dc83b56fd8ed976306ff2b7825c5a
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-11-08 12:35:38 +0100

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-# result = list(map(lambda x: x**2, col))
-# new_matrix.append(result)
-# square = [[x**2 for x in row] for row in matrix]
-    square = [x**2 for row in matrix for x in row]
+    '''
+    result = list(map(lambda x: x**2, col))
+    new_matrix.append(result)
+    '''
+    square = [[x**2 for x in row] for row in matrix]
     return square
--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-uniq_add = __import__('2-uniq_add').uniq_add
+square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
