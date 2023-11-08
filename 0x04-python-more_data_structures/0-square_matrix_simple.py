cda4c6943c5ac32c8b144e157f26c00fb08d292e
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-11-08 11:33:25 +0100

    Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+# result = list(map(lambda x: x**2, col))
+# new_matrix.append(result)
+    square = [[x**2 for x in row] for row in matrix]
+    return square
