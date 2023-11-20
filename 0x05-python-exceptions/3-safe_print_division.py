c8953571823f37dd1e01335041e73dd549480814
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-11-20 13:15:16 +0100

    Try Except Finally

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,11 +3,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (TypeError, ZeroDivisionError):
         result = None
     finally:
-        try:
-            print("Inside result: {: f}".format(result))
-        except (TypeError, ValueError):
             print("Inside result: {}".format(result))
     return result
