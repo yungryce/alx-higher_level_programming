0f3c47f527fbb29f7be3e8c36cc67fc3ab8e4a5b
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-11-20 15:22:22 +0100

    Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        print("Exception: {}".format(e))
