f278b083d750fee2726957146c9265df4134e217
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-11-20 13:05:34 +0100

    Try Except Finally

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return 1
+        return True
     except(TypeError, ValueError):
-        return 0
+        return False
