5374342cea1a3b4c49626c462d17ae98eb9637bf
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-11-27 14:27:13 +0100

    String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """class Rectangle that defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a class Rectangle
+        Arge:
+            width: width of a rectangle
+            height: height of a rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ defines an area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ define a perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """ returns a printable representation of a rectangle"""
+        result = ""
+        if self.__width != 0 and self.__height != 0:
+            for _ in range(self.__height):
+                result += '#' * self.__width + '\n'
+            return result.rstrip('\n')
