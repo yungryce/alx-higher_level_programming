98ef5b433895b5bf7eff61c5d35bf497cc8c2f59
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-12-04 14:01:37 +0100

    12. My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class MyInt that inherits from int:"""
+
+
+class MyInt(int):
+    """subclass of builtin int"""
+    def __eq__(self, other):
+        """ Invert the behavior of == """
+        return not super().__eq__(other)
+
+    def __ne__(self, other):
+        """ Invert the behavior of != """
+        return not super().__ne__(other)
