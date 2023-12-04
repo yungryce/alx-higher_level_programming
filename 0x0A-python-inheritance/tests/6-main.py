d5f7163590477044bbb0dff5c9efa997002a4ca1
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-12-04 11:35:41 +0100

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/tests/6-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+sys.path.append("..")
+BaseGeometry = __import__('6-base_geometry').BaseGeometry
+
+
+bg = BaseGeometry()
+
+try:
+    print(bg.area())
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
