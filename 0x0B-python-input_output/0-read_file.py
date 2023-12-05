6aead98dd4d803b498d51f44283cd90d3af59fa4
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-12-05 08:40:34 +0100

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    """"""
+    """reads a text file(UTF8) and prints it to stdout"""
     with open('my_file_0.txt', encoding="utf-8") as f:
-        return f.read()
+        print(f.read(), end='')
