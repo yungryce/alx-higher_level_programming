304aef5f34981696b978e58f158e65a08ac634da
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-12-05 08:57:40 +0100

    2. Append to a file

---
+++ 0x0B-python-input_output/tests/2-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+sys.path.append("..")
+
+
+append_write = __import__('2-append_write').append_write
+
+nb_characters_added = append_write("file_append.txt", "This School is so cool!\n")
+print(nb_characters_added)
