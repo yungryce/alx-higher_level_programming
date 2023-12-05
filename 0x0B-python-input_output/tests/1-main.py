f9072293333fe7a6f3841d3e405259b53f9a19bc
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-12-05 08:49:37 +0100

    1. Write to a file

---
+++ 0x0B-python-input_output/tests/1-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+sys.path.append("..")
+
+write_file = __import__('1-write_file').write_file
+
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
