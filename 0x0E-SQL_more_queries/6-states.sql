3caf31a2d255b11e0f23bba6a5b1f0ce3270d2cb
Chigbu Joshua <chigbujoshua@yahoo.com>
2024-01-17 18:07:03 +0100

    States table, Cities table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,3 +2,6 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS `states` (`id` INT AUTO_INCREMENT PRIMARY KEY, `name` VARCHAR(256) NOT NULL);
+
+-- Ensure id is unique and not null
+ALTER TABLE states MODIFY id INT UNIQUE NOT NULL AUTO_INCREMENT;
