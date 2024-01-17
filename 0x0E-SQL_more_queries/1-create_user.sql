7ddcea7153b85bfb4d21559659af1d2524931e7e
Chigbu Joshua <chigbujoshua@yahoo.com>
2024-01-17 16:57:40 +0100

    Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+--  script that creates the database hbtn_0d_2 and the user user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+FLUSH PRIVILEGES;
