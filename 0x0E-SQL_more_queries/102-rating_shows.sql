05dafdebaa5b0a34a9a2e13aca19a60af4640211
Chigbu Joshua <chigbujoshua@yahoo.com>
2024-01-18 04:58:58 +0100

    Rotten tomatoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,5 @@
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT title, SUM(rate) rating FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC
