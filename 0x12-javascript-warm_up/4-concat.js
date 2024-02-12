9af7e81aed1144328503919c378dd6218d8bff35
chxgbx <chigbujoshua@yahoo.com>
2024-02-12 14:27:21 +0000

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-const value_1 = process.argv[2];
-const value_2 = process.argv[3];
-console.log(`${value_1} is ${value_2}`);
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
