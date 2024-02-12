497d0722e808f600b7e5eaebbe1b8b6bc4ecbeae
chxgbx <chigbujoshua@yahoo.com>
2024-02-12 15:15:49 +0000

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const count = Number(process.argv[2]);
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+}
