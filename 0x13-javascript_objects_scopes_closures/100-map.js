9e3a68e45deba51fa5ad5c9dc080974ed03e6e05
chxgbx <chigbujoshua@yahoo.com>
2024-02-13 20:23:41 +0000

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const data = require('./100-data.js').list;
-// const mappedList = myList.map((value, index) => value * index);
+const mappedList = data.map((value, index) => value * index);
 // for (const index in myList) {
 //   myList[index] *= index;
 // }
-const newList = [];
-for (let i = 0; i < data.length; i++) {
-  newList.push(i * data[i]);
-}
+// const newList = [];
+// for (let i = 0; i < data.length; i++) {
+// newList.push(i * data[i]);
+// }
 console.log(data);
-console.log(newList);
+console.log(mappedList);
