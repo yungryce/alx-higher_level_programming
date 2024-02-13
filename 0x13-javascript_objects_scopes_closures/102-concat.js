3e759529df39630ab19009a5377a3295bad1d2c4
chxgbx <chigbujoshua@yahoo.com>
2024-02-13 20:16:57 +0000

    Concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+//const fArg = fs.readFileSync(process.argv[2]).toString();
+//const sArg = fs.readFileSync(process.argv[3]).toString();
+const fArg = fs.readFileSync(process.argv[2], 'utf8');
+const sArg = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], fArg + sArg);
