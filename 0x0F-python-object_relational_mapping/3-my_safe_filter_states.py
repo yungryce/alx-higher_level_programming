da5f79ba8a9d0b3d89a56b0a3d73da8afc52bed5
chxgbx <chigbujoshua@yahoo.com>
2024-02-15 17:03:23 +0000

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""handle for sql injetcions"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (sys.argv[4], ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
