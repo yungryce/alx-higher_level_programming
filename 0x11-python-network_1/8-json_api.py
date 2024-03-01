ace7b786ccea88e004e9791a4f38e2554add30b0
chxgbx <chigbujoshua@yahoo.com>
2024-03-01 15:34:14 +0000

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""""""
+
+import
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ''
+
+    try:
+        url = 'http://0.0.0.0:5000/search_user'
+        payload = {'q': q}
+        r = requests.post(url, payload).json()
+
+        if {'id', 'name'} <= r.keys():
+            print('[{id}] {name}'.format(id=r.get('id'), name=r.get('name')))
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
