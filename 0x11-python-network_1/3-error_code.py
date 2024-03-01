c79f4f393900c73a1184d90c7239ce0a47d5c15e
chxgbx <chigbujoshua@yahoo.com>
2024-03-01 13:14:04 +0000

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays the
+body of the response"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
