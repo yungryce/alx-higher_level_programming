1170c99c2158b41b859241c16ddfd1c1731e7da5
chxgbx <chigbujoshua@yahoo.com>
2024-03-01 13:21:20 +0000

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 # script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         html = res.read()
