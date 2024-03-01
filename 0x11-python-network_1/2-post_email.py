d42b7650871151d94640c455e90ba3a564f5693b
chxgbx <chigbujoshua@yahoo.com>
2024-03-01 13:09:21 +0000

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# URL with the email as a parameter, and displays the body of the response
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
