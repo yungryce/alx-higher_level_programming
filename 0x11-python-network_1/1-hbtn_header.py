eaf61fb2ffeca7a5996affd154a5e5a6bfafc2c7
chxgbx <chigbujoshua@yahoo.com>
2024-03-01 13:29:30 +0000

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,7 @@
 """script that takes in a URL, sends a request to the URL and displays the
 value of the X-Request-Id variable found in the header of the response."""
 
+
 import urllib.request
 import sys
 
