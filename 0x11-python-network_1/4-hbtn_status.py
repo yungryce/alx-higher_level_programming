082d7deb815ed5177eee8a1751b8a6a0dc8d510f
chxgbx <chigbujoshua@yahoo.com>
2024-03-01 15:06:13 +0000

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status."""
-import requests
-
 
 if __name__ == "__main__":
+    import requests
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
