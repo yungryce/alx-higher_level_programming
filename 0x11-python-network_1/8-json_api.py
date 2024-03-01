57cdfcb6d4fbb966adb078a134d47ea6237ad597
chxgbx <chigbujoshua@yahoo.com>
2024-03-01 15:41:10 +0000

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""""""
+"""Python script that takes a letter and sends a POST request to 
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
 
-import
+import sys
+import requests
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        q = argv[1]
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
     else:
         q = ''
 
